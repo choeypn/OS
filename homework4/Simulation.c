81da2fa5fb99016222670393fa563a0c3260eb5e
Natthapong None Choeypant <choeypn@cf405-13.cs.wwu.edu>
2020-03-12 15:15:08 -0700

    so far: created an inverted page table

--- homework4/Simulation.c
+++ homework4/Simulation.c
@@ -1,24 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* If there are custom classes/source files that you write, with
-   custom functions, and you want those functions available for use in
-   THIS .c file, then include the header file for the custom .c
-   file(s) you've written, using the #include directive. For example:
-
-   #include "SomeFile.h"
-
- */
-
-void Simulate(char* fileName1, char* fileName2, char allocation) {
-  // A function whose inputs are the trace files names and the allocation 
-  // strategy 's' for split or 'f' for free for all
-
+//#include "simulation.h"
   
+//a structure for pagetable
+typedef struct pageTable{
+  int* timeStamp;
+  char** pageNumber;
+  int* ASID;
+}pageTable;
+
+//get input arg for allocation
+//return allocation type from input arg
+char* getAllocationMode(char in){
+  char *alc;
+  if(in == 's')
+    alc = "Spllit";
+  else if(in == 'f')
+    alc = "Free";
+  else
+    alc = "Invalid";
+  return alc;
 }
 
-int main() {
+pageTable* initializePageTable(){
+  pageTable* invertedTable = malloc(sizeof(pageTable));
+  invertedTable->timeStamp = malloc(sizeof(int)*4096);
+  invertedTable->pageNumber = malloc(4096*(sizeof(char)*8));
+  invertedTable->ASID = malloc(sizeof(int)*4096);
+  for(int i = 0; i < 4096; i++){
+    invertedTable->timeStamp[i] = -1;
+    invertedTable->pageNumber[i] = "-1";
+    invertedTable->ASID[i] = -1;
+  }
+  return invertedTable;
+}
+
+// A function whose inputs are the trace files names and the allocation 
+// strategy 's' for split or 'f' for free for all 
+void Simulate(char* fileName1, char* fileName2, char allocation) {
+  int state = 1;
+  char *allc = getAllocationMode(allocation);
+  FILE* f1;
+  FILE* f2;
+  f1 = fopen(fileName1,"r");
+  f2 = fopen(fileName2,"r");
+  if(f1 == NULL || f2 == NULL){
+    puts("invalid input file");
+    state = 0;
+  }
+  if(state){
+    pageTable* inv = initializePageTable();  
+
+//    printf("%d : %s : %d \n",inv->timeStamp[2541],inv->pageNumber[2541],inv->ASID[2541]);
+    free(inv);
+    fclose(f1);
+    fclose(f2);
+    printf("%s \n",allc);
+  }
+}
+
+int main(int argc, char** argv) {
   // Run simulation
-
-
+  if(argc != 4){
+    puts("Incorrect number of input args. Exit");
+    return(0);
+  }
+  char *firstFile = argv[1];
+  char *secondFile = argv[2];
+  char mode = *argv[3];
+  Simulate(firstFile,secondFile,mode);  //run simulation here
+  exit(0);
 }
